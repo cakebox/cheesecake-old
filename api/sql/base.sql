65afba5b8760bdf7f82d60c2efda2985d4c17e8e
Tuxity <kevin.darcel@gmail.com>
2015-01-02 18:47:50 -0600

    API: basic test accounta with their rights user / user modo / modo admin / admin

--- api/sql/base.sql
+++ api/sql/base.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # http://code.google.com/p/sequel-pro/
 #
-# Hôte: localhost (MySQL 10.0.14-MariaDB)
+# Hôte: localhost (MySQL 10.0.15-MariaDB)
 # Base de données: colabsubs
-# Temps de génération: 2014-12-31 21:13:27 +0000
+# Temps de génération: 2015-01-03 00:45:20 +0000
 # ************************************************************
 
 
@@ -39,8 +39,19 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `age`, `gender`, `last_seen`, `last_ip`, `subscription_date`, `failed_logins`)
+VALUES
+	(1,'user','$2y$10$AHlCzRePTRmzTKmb.WbZROznKmHGCpnmjErwfs2NngcYHk1lnVqOG','user@user.user',NULL,NULL,'2015-01-02 18:43:59','127.0.0.1','2015-01-02 18:43:59',0),
+	(2,'modo','$2y$10$l9IQju8HMi30izZukfkml.ecV/fjLFs5wsEV/gVIiAU.CBfFAh3yK','modo@modo.modo',NULL,NULL,'2015-01-02 18:44:16','127.0.0.1','2015-01-02 18:44:16',0),
+	(3,'admin','$2y$10$sf1krqI89gRZQLo6L5E4aeTqaxa3DXCaZFKaBJ3/Xj2NlpfaZAOxq','admin@admin.admin',NULL,NULL,'2015-01-02 18:44:32','127.0.0.1','2015-01-02 18:44:32',0);
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Affichage de la table users_access
@@ -55,6 +66,16 @@ CREATE TABLE `users_access` (
   CONSTRAINT `users_access_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `users_access` WRITE;
+/*!40000 ALTER TABLE `users_access` DISABLE KEYS */;
+
+INSERT INTO `users_access` (`user_id`, `user_level`)
+VALUES
+	(2,1),
+	(3,2);
+
+/*!40000 ALTER TABLE `users_access` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Affichage de la table users_avatar

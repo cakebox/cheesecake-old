498e02489b43e6ee07db358e232e8c62b639c3b6
Tuxity <kevin.darcel@gmail.com>
2015-01-06 12:21:02 -0600

    API: More work on databse, still have improvements to do

--- api/sql/base.sql
+++ api/sql/base.sql
@@ -7,7 +7,7 @@
 #
 # Hôte: localhost (MySQL 10.0.15-MariaDB)
 # Base de données: colabsubs
-# Temps de génération: 2015-01-03 00:45:20 +0000
+# Temps de génération: 2015-01-06 18:20:34 +0000
 # ************************************************************
 
 
@@ -20,6 +20,151 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
+# Affichage de la table movies
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `movies`;
+
+CREATE TABLE `movies` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `imdb_id` int(11) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `year` int(4) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table movies_subtitles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `movies_subtitles`;
+
+CREATE TABLE `movies_subtitles` (
+  `movie_id` int(11) unsigned NOT NULL,
+  `subtitle_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`movie_id`,`subtitle_id`),
+  KEY `subtitle_id` (`subtitle_id`),
+  CONSTRAINT `movies_subtitles_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`),
+  CONSTRAINT `movies_subtitles_ibfk_2` FOREIGN KEY (`subtitle_id`) REFERENCES `subtitles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table subtitles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `subtitles`;
+
+CREATE TABLE `subtitles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `language` varchar(255) NOT NULL,
+  `high_definition` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `audio_description` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `tags` varchar(255) DEFAULT '',
+  `is_release` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table subtitles_in_progress
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `subtitles_in_progress`;
+
+CREATE TABLE `subtitles_in_progress` (
+  `subtitle_id` int(11) unsigned NOT NULL,
+  `sentence_id` int(11) unsigned NOT NULL,
+  `sentence` text,
+  `created_by` int(11) unsigned NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`subtitle_id`,`sentence_id`,`created_by`),
+  KEY `created_by` (`created_by`),
+  CONSTRAINT `subtitles_in_progress_ibfk_1` FOREIGN KEY (`subtitle_id`) REFERENCES `subtitles` (`id`),
+  CONSTRAINT `subtitles_in_progress_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table subtitles_released
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `subtitles_released`;
+
+CREATE TABLE `subtitles_released` (
+  `subtitle_id` int(11) unsigned NOT NULL,
+  `version` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `subtitle` mediumtext,
+  PRIMARY KEY (`subtitle_id`),
+  CONSTRAINT `subtitles_released_ibfk_1` FOREIGN KEY (`subtitle_id`) REFERENCES `subtitles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table team
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `team`;
+
+CREATE TABLE `team` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `website` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table team_users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `team_users`;
+
+CREATE TABLE `team_users` (
+  `team_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`team_id`,`user_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `team_users_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`),
+  CONSTRAINT `team_users_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table tvshows
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tvshows`;
+
+CREATE TABLE `tvshows` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `tvdb_id` int(11) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `season` tinyint(3) unsigned NOT NULL,
+  `episode` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table tvshows_subtitles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tvshows_subtitles`;
+
+CREATE TABLE `tvshows_subtitles` (
+  `tvshow_id` int(11) unsigned NOT NULL,
+  `subtitle_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`tvshow_id`,`subtitle_id`),
+  KEY `subtitle_id` (`subtitle_id`),
+  CONSTRAINT `tvshows_subtitles_ibfk_1` FOREIGN KEY (`tvshow_id`) REFERENCES `tvshows` (`id`),
+  CONSTRAINT `tvshows_subtitles_ibfk_2` FOREIGN KEY (`subtitle_id`) REFERENCES `subtitles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
 # Affichage de la table users
 # ------------------------------------------------------------
 
@@ -30,12 +175,12 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL DEFAULT '',
-  `age` tinyint(3) DEFAULT NULL,
-  `gender` tinyint(1) DEFAULT NULL,
+  `age` tinyint(3) unsigned DEFAULT NULL,
+  `gender` tinyint(1) unsigned DEFAULT NULL,
   `last_seen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_ip` varchar(15) NOT NULL DEFAULT '',
   `subscription_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `failed_logins` int(11) NOT NULL DEFAULT '0',
+  `failed_logins` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
@@ -46,9 +191,9 @@ LOCK TABLES `users` WRITE;
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `age`, `gender`, `last_seen`, `last_ip`, `subscription_date`, `failed_logins`)
 VALUES
-	(1,'user','$2y$10$AHlCzRePTRmzTKmb.WbZROznKmHGCpnmjErwfs2NngcYHk1lnVqOG','user@user.user',NULL,NULL,'2015-01-02 18:43:59','127.0.0.1','2015-01-02 18:43:59',0),
-	(2,'modo','$2y$10$l9IQju8HMi30izZukfkml.ecV/fjLFs5wsEV/gVIiAU.CBfFAh3yK','modo@modo.modo',NULL,NULL,'2015-01-02 18:44:16','127.0.0.1','2015-01-02 18:44:16',0),
-	(3,'admin','$2y$10$sf1krqI89gRZQLo6L5E4aeTqaxa3DXCaZFKaBJ3/Xj2NlpfaZAOxq','admin@admin.admin',NULL,NULL,'2015-01-02 18:44:32','127.0.0.1','2015-01-02 18:44:32',0);
+	(1,'user','$2y$10$AHlCzRePTRmzTKmb.WbZROznKmHGCpnmjErwfs2NngcYHk1lnVqOG','user@user.user',NULL,NULL,'0000-00-00 00:00:00','127.0.0.1','2015-01-02 18:43:59',0),
+	(2,'modo','$2y$10$l9IQju8HMi30izZukfkml.ecV/fjLFs5wsEV/gVIiAU.CBfFAh3yK','modo@modo.modo',NULL,NULL,'0000-00-00 00:00:00','127.0.0.1','2015-01-02 18:44:16',0),
+	(3,'admin','$2y$10$sf1krqI89gRZQLo6L5E4aeTqaxa3DXCaZFKaBJ3/Xj2NlpfaZAOxq','admin@admin.admin',NULL,NULL,'0000-00-00 00:00:00','127.0.0.1','2015-01-02 18:44:32',0);
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -61,7 +206,7 @@ DROP TABLE IF EXISTS `users_access`;
 
 CREATE TABLE `users_access` (
   `user_id` int(11) unsigned NOT NULL,
-  `user_level` tinyint(1) NOT NULL,
+  `user_level` tinyint(1) unsigned NOT NULL,
   PRIMARY KEY (`user_id`),
   CONSTRAINT `users_access_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -102,10 +247,12 @@ DROP TABLE IF EXISTS `users_banned`;
 CREATE TABLE `users_banned` (
   `user_id` int(11) unsigned NOT NULL,
   `ban_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `by` int(11) NOT NULL,
+  `by` int(11) unsigned NOT NULL,
   `reason` text NOT NULL,
   PRIMARY KEY (`user_id`),
-  CONSTRAINT `users_banned_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  KEY `by` (`by`),
+  CONSTRAINT `users_banned_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `users_banned_ibfk_2` FOREIGN KEY (`by`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -132,10 +279,10 @@ DROP TABLE IF EXISTS `users_stats`;
 
 CREATE TABLE `users_stats` (
   `user_id` int(11) unsigned NOT NULL,
-  `karma` float NOT NULL DEFAULT '0',
-  `translated_lines` int(11) NOT NULL DEFAULT '0',
-  `uploaded_subs` int(11) NOT NULL DEFAULT '0',
-  `edited_subs` int(11) NOT NULL DEFAULT '0',
+  `karma` float unsigned NOT NULL DEFAULT '0',
+  `translated_lines` int(11) unsigned NOT NULL DEFAULT '0',
+  `uploaded_subs` int(11) unsigned NOT NULL DEFAULT '0',
+  `edited_subs` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`),
   CONSTRAINT `users_stats_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
